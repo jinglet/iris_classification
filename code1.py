8df7df2ac3491b1b8236661e91614323fe451ac5
Han JS <thlwjdnf02@naver.com>
2023-01-09 17:11:58 +0900

    iris.csv 수정

--- code1.py
+++ code1.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 
 iris_data = load_iris()
-print(iris_data.keys())
+#print(iris_data.keys())
+#print(iris_data['feature_names'], iris_data['target_names'])
 
-iris_data = pd.DataFrame(data=iris_data['data'])
+iris_data = pd.DataFrame(data=iris_data['data'], columns=iris_data['feature_names'])
 
 iris_data.to_csv('iris.csv')
+print(iris_data)
